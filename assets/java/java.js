db9b7c30df70795aa2d3662d07b8168116ae24c4
NCSchmidt <nicklas.c.schmidt@gmail.com>
2018-07-16 20:43:04 -0700

    don't push

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,15 +1,9 @@
 
-var startDate = "2017-02-13";
-var endDate = "2017-02-13";
-var apiKey = "bdca922fc5166acd71413f149462c1a2a01766f862a8cbcb93de2b1cd79df47d";
-var oddsURL = "https://apifootball.com/api/?action=get_odds&from=" + startDate + "&to=" + endDate + "&APIkey=" + apiKey;
-var headToHeadURL = "https://apifootball.com/api/?action=get_H2H&firstTeam=Chelsea&secondTeam=Arsenal&APIkey=" + apiKey;
 
 var testURL = "https://apifootball.com/api/?action=get_odds&APIkey=" + apiKey + "&match_id=297289";
 
 var myEventID = "25861179737";
 var myLeagueID = "20336";
-var mashapeURL = "https://bettingodds-bettingoddsapi-v1.p.mashape.com/events/2018-07-12";
 var mashapeEventURL = "https://bettingodds-bettingoddsapi-v1.p.mashape.com/event/" + myEventID;
 var mashapeLeagueURL = "https://bettingodds-bettingoddsapi-v1.p.mashape.com/events/league/" + myLeagueID;
 
@@ -87,6 +81,8 @@ $.ajax({
         gameArrayFinal.push(gameArrayObjects[randomNum]);
     }
     console.log(gameArrayFinal);
+
+
 });
 
 // Generates the list of leagues using hardcoded IDs
@@ -188,6 +184,17 @@ $(document).ready(function () {
 
 
 
+
+var currentGameID;
+var currentGameNum  = 0;
+var currentGameObject = gameArrayFinal[currentGameNum];
+var currentGameID = currentGameObject.id;
+
+var matchIdArray = []
+
+for (var n=0; n < 3; n++) {
+    
+}
 
 
 
